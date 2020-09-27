df2070122a4cccfb90b915202435079b8561c5ac
kkristof200 <kovacskristof200@gmail.com>
2020-09-27 03:06:26 +0300

    no message

--- src/extensions/string.ts
+++ src/extensions/string.ts
@@ -9,7 +9,7 @@ declare global {
     }
 }
 String.prototype.filled = function (length: number, side: FillSide = FillSide.Left, fillChar: string = ' ') {
-    if (this.length <= length) return this
+    if (this.length >= length) return this
     if (side == FillSide.Right) return this + fillChar.repeat(length-this.length)
     
     return  fillChar.repeat(length-this.length) + this
