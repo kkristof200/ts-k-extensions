8fae976b728c97759e4ee9c9b9c4b447277d4911
kkristof200 <kovacskristof200@gmail.com>
2020-10-02 20:12:46 +0300

    no message

--- src/extensions/string.ts
+++ src/extensions/string.ts
@@ -7,9 +7,10 @@ declare global {
     interface String {
         zfill(length: number, fillChar?: string, side?: FillSide): string
         filled(length: number, fillChar?: string, side?: FillSide): string
-        regexReplace(regexStr: string, toReplaceWith?: string): string
+        remove(toRemove: string): string
+        replaceAll(toReplace: string, toReplaceWith?: string): string
         trimRight(charlist?: string): string
-        trimleft(charlist?: string): string
+        trimLeft(charlist?: string): string
         trim(charlist?: string): string
         lastPathComponent(): string
     }
@@ -20,17 +21,34 @@ String.prototype.zfill = function(length: number, fillChar: string = ' ', side: 
 }
 String.prototype.filled = function(length: number, fillChar: string = ' ', side: FillSide = FillSide.Left) {
     if (this.length >= length) return this
-    
+
     return side == FillSide.Right ? this + fillChar.repeat(length-this.length) : fillChar.repeat(length-this.length) + this
 }
-String.prototype.trimRight = function(charlist?: string) { return this.regexReplace('[' + charlist ?? '\s' + ']+$') }
-String.prototype.trimLeft = function(charlist?: string) { return this.regexReplace('^[' + charlist ?? '\s' + ']+') }
-String.prototype.trim = function(charlist?: string) {
-    if (!charlist) charlist = '\s'
-  
-    return this.trimLeft(charlist).trimRight(charlist)
+String.prototype.remove = function(toRemove: string) { return this.replaceAll(toRemove) }
+String.prototype.replaceAll = function(toReplace: string, toReplaceWith?: string) {
+    return this.split(toReplace).join(toReplaceWith ?? '')
 }
-String.prototype.regexReplace = function(regexStr: string, toReplaceWith?: string) {  
-    return this.replace(new RegExp(regexStr), toReplaceWith ?? '')
+String.prototype.trimRight = function(chars?: string) {
+    if (!chars || chars.length == 0) chars = ' '
+
+    var newStr = `${this}`
+
+    while (newStr.endsWith(chars)) {
+        newStr = newStr.substr(0, newStr.length - chars.length)
+    }
+
+    return newStr
 }
+String.prototype.trimLeft = function(chars?: string) {
+    if (!chars || chars.length == 0) chars = ' '
+
+    var newStr = `${this}`
+
+    while (newStr.startsWith(chars)) {
+        newStr = newStr.substr(chars.length - 1, newStr.length - chars.length)
+    }
+    
+    return newStr
+}
+String.prototype.trim = function(chars?: string) { return this.trimLeft(chars).trimRight(chars) }
 String.prototype.lastPathComponent = function(): string { return this.trimRight('/').split('/').last }
